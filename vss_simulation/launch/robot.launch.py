cef85f51f113698424c293436b90ef1bfa1bc0f7
Leo <souleonardo1@gmail.com>
2025-08-18 19:39:16 -0300

    Refiz o robot.launch.py para reutilizar o codigo de criacao deles(nao repetir cada vez que for necessario criar um novo robo)

--- vss_simulation/launch/robot.launch.py
+++ vss_simulation/launch/robot.launch.py
@@ -1,6 +1,7 @@
 import os
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
+from launch.actions import GroupAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch.substitutions import Command, PathJoinSubstitution
@@ -9,78 +10,83 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
+     robots_z = 0.1
+     robots_Y = -1.57
 
-    pkg_dir = get_package_share_directory('vss_simulation')
+     pkg_dir = get_package_share_directory('vss_simulation')
 
+     robot_path = os.path.join(pkg_dir, 'urdf', 'vss.xacro')
+     camera_file = PathJoinSubstitution([pkg_dir, 'urdf', 'camera.urdf.xacro'])
 
-    robot_path = os.path.join(pkg_dir, 'urdf', 'vss.xacro')
-    camera_file = PathJoinSubstitution([pkg_dir, 'urdf', 'camera.urdf.xacro'])
+     robots = [
+          [
+               'robot_team1_center',
+               [0.375, 0]
+          ], 
+          [
+               'robot_team1_north',
+               [0.375, 0.4]
+          ], 
+          [
+               'robot_team1_south',
+               [0.375, -0.4]
+          ],
+          [
+               'robot_team2_center',
+               [-0.375, 0]
+          ], 
+          [
+               'robot_team2_north',
+               [-0.375, 0.4]
+          ], 
+          [
+               'robot_team2_south',
+               [-0.375, -0.4]
+          ]]
 
+     ld = LaunchDescription([IncludeLaunchDescription(
+          PythonLaunchDescriptionSource([
+               pkg_dir, '/launch/arena_vss.launch.py'
+          ])
+     )])
 
-    return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
-                pkg_dir, '/launch/arena_vss.launch.py'
-            ]),
-        ),
+     for name, pos in robots:
+          robot_state_publisher = Node(
+               package='robot_state_publisher', 
+               executable='robot_state_publisher',
+               namespace=name, 
+               parameters=[{'robot_description': Command(['xacro ', robot_path])}])
+          
+          spawn_entity = Node(
+               package='gazebo_ros', 
+               executable='spawn_entity.py',
+               arguments=['-entity', name, '-topic', f'/{name}/robot_description', 
+                    '-x', str(pos[0]), '-y', str(pos[1]), '-z', str(robots_z), '-Y', str(robots_Y)])
 
-        Node(package='robot_state_publisher', 
-             executable='robot_state_publisher',
-             namespace='robot_team1_center', parameters=[{'robot_description': Command(['xacro ', robot_path])}]),
-        Node(package='gazebo_ros', 
-             executable='spawn_entity.py',
-             arguments=['-entity', 'robot_team1_center', '-topic', '/robot_team1_center/robot_description', '-x', '0.375', '-y', '0', '-z', '0.1', '-Y', '-1.57']),
+          ld.add_action(GroupAction([
+               robot_state_publisher,
+               spawn_entity
+          ]))
 
-        Node(package='robot_state_publisher', 
-             executable='robot_state_publisher',
-             namespace='robot_team1_north', parameters=[{'robot_description': Command(['xacro ', robot_path])}]),
-        Node(package='gazebo_ros', 
-             executable='spawn_entity.py',
-             arguments=['-entity', 'robot_team1_north', '-topic', '/robot_team1_north/robot_description', '-x', '0.375', '-y', '0.4', '-z', '0.1', '-Y', '-1.57']),
+     robot_state_publisher_camera = Node(
+          package='robot_state_publisher',
+          executable='robot_state_publisher',
+          name='robot_state_publisher',
+          namespace='camera',
+          output='screen',
+          parameters=[
+               {'robot_description': ParameterValue(Command(['xacro ', camera_file]), value_type=str)}
+          ])
+     spawn_entity_camera = Node(
+          package='gazebo_ros',
+          executable='spawn_entity.py',
+          arguments=['-entity', 'up_camera', '-topic', '/camera/robot_description', '-x', '0', '-y', '0', '-z', '2', '-P', '1.57'],
+          output='screen'
+     )
 
-        Node(package='robot_state_publisher', 
-             executable='robot_state_publisher',
-             namespace='robot_team1_south', parameters=[{'robot_description': Command(['xacro ', robot_path])}]),
-        Node(package='gazebo_ros', 
-             executable='spawn_entity.py',
-             arguments=['-entity', 'robot_team1_south', '-topic', '/robot_team1_south/robot_description', '-x', '0.375', '-y', '-0.4', '-z', '0.1', '-Y', '-1.57']),
+     ld.add_action(GroupAction([
+          robot_state_publisher_camera,
+          spawn_entity_camera
+     ]))
 
-        Node(package='robot_state_publisher', 
-             executable='robot_state_publisher',
-             namespace='robot_team2_center', parameters=[{'robot_description': Command(['xacro ', robot_path])}]),
-        Node(package='gazebo_ros', 
-             executable='spawn_entity.py',
-             arguments=['-entity', 'robot_team2_center', '-topic', '/robot_team2_center/robot_description', '-x', '-0.375', '-y', '0', '-z', '0.1', '-Y', '1.57']),
-
-        Node(package='robot_state_publisher', 
-             executable='robot_state_publisher',
-             namespace='robot_team2_north', parameters=[{'robot_description': Command(['xacro ', robot_path])}]),
-        Node(package='gazebo_ros', 
-             executable='spawn_entity.py',
-             arguments=['-entity', 'robot_team2_north', '-topic', '/robot_team2_north/robot_description', '-x', '-0.375', '-y', '0.4', '-z', '0.1', '-Y', '1.57']),
-
-        Node(package='robot_state_publisher', 
-             executable='robot_state_publisher',
-             namespace='robot_team2_south', parameters=[{'robot_description': Command(['xacro ', robot_path])}]),
-        Node(package='gazebo_ros', 
-             executable='spawn_entity.py',
-             arguments=['-entity', 'robot_team2_south', '-topic', '/robot_team2_south/robot_description', '-x', '-0.375', '-y', '-0.4', '-z', '0.1', '-Y', '1.57']),
-
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            namespace='camera',
-            output='screen',
-            parameters=[
-                {'robot_description': ParameterValue(Command(['xacro ', camera_file]), value_type=str)}
-            ]
-        ),
-        Node(
-            package='gazebo_ros',
-            executable='spawn_entity.py',
-            arguments=['-entity', 'up_camera', '-topic', '/camera/robot_description', '-x', '0', '-y', '0', '-z', '2', '-P', '1.57'],
-            output='screen'
-        )
-        
-    ])
+     return ld
